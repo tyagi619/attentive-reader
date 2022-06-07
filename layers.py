062056d2c4e3620c23dd064090bab22c2234050e
Anubhav Tyagi <tyagianubhav619@gmail.com>
2022-06-08 01:33:03 +0530

    Added CharEmbedding layer
    Added code for building word embeddings using character level
    embeddings and Convolution Neural Network. The layer uses CNN
    to learn character interactions and build word embeddings

--- layers.py
+++ layers.py
@@ -8,7 +8,6 @@ model for question answering.
 5- BiDAFOutput
 '''
 
-from sklearn.preprocessing import scale
 import torch
 import torch.nn  as nn
 import torch.nn.functional as F
@@ -34,6 +33,24 @@ class Highway(nn.Module):
         return x
 
 
+class CNN(nn.Module):
+    def __init__(self, e_char, e_word, len_chars):
+        super(CNN, self).__init__()
+        self.kernel = 5
+
+        self.conv1d = nn.Conv1d(in_channels=e_char,
+                                out_channels=e_word,
+                                kernel=self.kernel,
+                                bias=True)
+        self.maxPool = nn.MaxPool1d(kernel_size=len_chars-self.kernel+1)
+
+    def forward(self, x):
+        x_conv = self.conv1d(x)
+        x_conv = F.relu(x_conv)
+        x_conv = self.maxPool(x_conv).squeeze(dim=-1)
+        return x_conv
+
+
 class WordEmbedding(nn.Module):
     def __init__(self, word_vectors, num_highway_layers, hidden_size, drop_prob):
         super(WordEmbedding, self).__init__()
@@ -57,11 +74,28 @@ class WordEmbedding(nn.Module):
 
 
 class CharEmbedding(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, char_vectors, e_char, e_word, len_chars, num_highway_layers, drop_prob):
+        super(CharEmbedding, self).__init__()
+        self.kernel = 5
+        self.drop_prob = drop_prob
+        
+        self.embed = nn.Embedding.from_pretrained(embeddings=char_vectors,
+                                                  freeze=False,
+                                                  padding_idx=0,
+                                                  scale_grad_by_freq=False)
+        self.cnn = CNN(e_char, e_word, len_chars)
+        self.highway = Highway(num_highway_layers, e_word)
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        x_embed = self.embed(x)
+        x_reshaped = x_embed.reshape(x_embed.size(0)*x_embed.size(1),
+                                     x_embed.size(3), x_embed.size(2))
+        x_conv = self.cnn(x_reshaped)
+        x_conv = x_conv.reshape(x.size(0), x.size(1), x_conv.size(1))
+        x_out = self.highway(x_conv)
+        x_out = F.dropout(input=x_out, p=self.drop_prob,
+                          training=self.training, inplace=False)
+        return x_out
 
 
 class RNNEncoder(nn.Module):
